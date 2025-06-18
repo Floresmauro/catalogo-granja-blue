5e9c949d77160f1826fa6a9d7c31a79d2c933a82
Floresmauro <165447698+Floresmauro@users.noreply.github.com>
2025-06-18 14:30:31 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 👇 Este base es esencial para GitHub Pages
 export default defineConfig({
+  base: '/catalogo-granja-blue/',
   plugins: [react()]
 })
