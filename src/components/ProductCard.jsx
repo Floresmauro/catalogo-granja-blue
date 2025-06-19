6c4b96ae51576de61cd5e328a327835f0e6c344d
Floresmauro <mauroaleflores@gmail.com>
2025-06-18 21:35:38 -0300

    Update ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,13 @@
 export default function ProductCard({ nombre, precio, imagen, onAgregar }) {
   return (
     <div className="bg-white shadow rounded p-4 text-center">
-      <img src={imagen} alt={nombre} className="w-32 h-32 mx-auto object-cover mb-2 rounded" />
+      <div className="w-full h-48 mb-3 overflow-hidden rounded">
+        <img
+          src={imagen}
+          alt={nombre}
+          className="w-full h-full object-cover mx-auto"
+        />
+      </div>
       <h3 className="text-lg font-bold">{nombre}</h3>
       <p className="text-gray-700 mb-2">${precio}</p>
       <button
